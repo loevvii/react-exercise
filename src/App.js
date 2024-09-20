249e7fcaba5170084482e9864addedefb34687e8
Louis Vinas <lhvinas112203@gmail.com>
2024-09-20 15:34:20 +0800

    added portfolioo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/navbar.js';
 import Profile from './components/profile.js'
 import './components/css/profilestyle.css'
+import Portfolio from './components/portfolio.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <div>
       <Navbar/>
       <Profile/>
+      <Portfolio/>
     </div>
   );
 }
