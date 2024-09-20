844ecd11dcc09b24610dead44626ead0cb244119
Louis Vinas <lhvinas112203@gmail.com>
2024-09-20 15:25:30 +0800

    updated

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,21 +4,18 @@ const Profile = () => {
     return (
         <section className="jumbotron jumbotron-fluid pt-3 d-flex justify-content-right align-items-right vh-100">
             <div className="grid-container">
-                <div
-                    class="row justify-content-center align-items-center g-2"
-                >
+                <div class="row justify-content-center align-items-center g-2">
                     <div class="col"><div className='grid-item'><img 
                     className="disc" 
                     src="https://m.media-amazon.com/images/I/717UvyG+NXL._UF1000,1000_QL80_.jpg"  alt="Profile"  />
                     </div></div>
                     <div class="col"><div className='grid-item'>
-                <h1>text</h1>
-                </div></div>
-                    <div class="col">Column</div>
-                </div>
-                
-                <div className='grid-item'>
-                <h1>text</h1>
+                    <h1></h1>
+                    </div></div>
+                    <div class="col"><p>Schwarz</p>Hello</div>
+                    </div>
+                    <div className='grid-item'>
+                        <div class="row">some text</div>
                 </div>
             </div>
         </section>
